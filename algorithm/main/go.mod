084f97fdff15ebb1f2f898874093ebb4f50bc5c2
hujinrun <hujinrunliyanjing@gmail.com>
2022-05-22 22:23:48 +0800

    --story=添加图的bfs和dfs接口

--- algorithm/main/go.mod
+++ algorithm/main/go.mod
@@ -4,4 +4,9 @@ go 1.18
 
 replace union_find => ../union_find
 
-require union_find v0.0.0-00010101000000-000000000000 // indirect
+replace graph/base => ../graph/base
+
+require (
+	graph/base v0.0.0-00010101000000-000000000000 // indirect
+	union_find v0.0.0-00010101000000-000000000000 // indirect
+)
--- algorithm/main/main.go
+++ algorithm/main/main.go
@@ -1,12 +1,16 @@
 package main
 
-import (
-	"fmt"
-
-	"union_find"
-)
+import "graph/base"
 
 func main() {
-	a := union_find.SimpleUnion[int]{}
-	fmt.Println(a)
+	var graph base.IGraph
+	graph = &base.MatrixUndirectedGraph{}
+	graph.InitGraph(5)
+
+	graph.InsertEdge(0, 1, 1)
+	graph.InsertEdge(0, 2, 2)
+	graph.InsertEdge(1, 3, 1)
+	graph.InsertEdge(2, 4, 1)
+	//graph.InsertEdge(3, 4, 2)
+	graph.TravelGraphUsingDFS()
 }
